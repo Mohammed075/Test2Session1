126bf1d45b2e8459af34bfcb189621ce0e581a55
Mohammed075 <94587512+Mohammed075@users.noreply.github.com>
2022-03-17 04:21:38 +0000

    P1

--- p1original.c
+++ p1original.c
@@ -3,24 +3,24 @@
 void input(float*x1, float*x2, float*y1, float*y2)
 {
   printf("Enter the value\n");
-  scanf("%f%f%f%f",x1,x2,y1,y2);
+  scanf("%f%f",x1,y1);
+  printf("enter the coordinates of second point");
+  scanf("%f%f",x2,y2);
 }
-void find_output(float x1, float x2, float y1,float y2, float*area)
+void find_output(float x1, float x2, float y1,float y2, float*distance)
 {
-  *area=sqrt (pow(x2-x1,2)+pow(y2-y1,2));
+  *distance=sqrt(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
 }
-void output(float x1, float x2, float y1, float y2, float area)
+void output(float x1, float x2, float y1, float y2, float distance)
 {
-  printf("therefore the distance between points are %f",area);
-
-  
+    printf("The distance between the points (%f,%f) and (%f,%f) is %f\n",x1,y1,x2,y2,distance);
 }
 int main()
 {
-  float x1,x2,y1,y2,area;
+  float x1,x2,y1,y2,distance;
   input(&x1,&x2,&y1,&y2);
-  find_output(x1,x2,y1,y2,&area);
-  output(x1,x2,y1,y2,area);
+  find_output(x1,x2,y1,y2,&distance);
+  output(x1,x2,y1,y2,distance);
   return 0; 
 
 }
